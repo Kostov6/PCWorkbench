5e248af33cfa0b77276b0982c1920ef6815a3a52
Kostov6 <angliyskiwiktor@gmail.com>
2020-05-30 14:54:51 +0300

    Initial test commit

---
+++ js/logic.js
@@ -0,0 +1,106 @@
+var parts = 0;
+var price = 0;
+var wattage = 0;
+var chosenComponents = new Array();
+
+class component {
+  constructor(name, type, wattage, price) {
+    this.name = name;
+    this.type = type;
+    this.wattage = wattage;
+    this.price = price;
+  }
+}
+
+chosenComponents["CPU"] = "";
+chosenComponents["GPU"] = "";
+chosenComponents["power"] = "";
+chosenComponents["fan"] = "";
+chosenComponents["motherBoard"] = "";
+chosenComponents["RAM"] = "";
+chosenComponents["HDD"] = "";
+chosenComponents["chassis"] = "";
+
+function update() {
+  document.getElementById("parts").innerText = parts;
+  document.getElementById("price").innerText = "$" + price;
+  document.getElementById("wattage").innerText = wattage;
+  validPowerSupplies();
+}
+
+function validPowerSupplies() {
+  validNames = new Array();
+  invalidNames = new Array();
+  for (i = 0; i < allComponents.length; i++) {
+    current = allComponents[i];
+    if (current.type == "power" && current.data1.split(" ")[0] < wattage) {
+      //invalid
+      invalidNames.push(current.name);
+    }
+    if (current.type == "power" && current.data1.split(" ")[0] >= wattage) {
+      //valid
+      validNames.push(current.name);
+    }
+  }
+
+  allNodeComponents = document.getElementById("components").children;
+  for (i = 0; i < allNodeComponents.length; i++) {
+    current = allNodeComponents[i];
+    var name = current.querySelector("a[name='name']").innerText;
+
+    if (invalidNames.includes(name)) {
+      //invalidate
+      current.children[5].children[0].classList.add("d-none");
+      current.children[5].children[1].classList.remove("d-none");
+    }
+    if (validNames.includes(name)) {
+      //validate
+      current.children[5].children[0].classList.remove("d-none");
+      current.children[5].children[1].classList.add("d-none");
+    }
+  }
+}
+
+function showErrorMsg(event) {
+  // alert("ShowError");
+  // console.log("ShowError");
+}
+
+function restoreContent(event) {
+  //alert("RestoreContent v");
+  // console.log("RestoreContent v");
+}
+
+function showErrorMsg() { }
+
+function remove(event) {
+  var id = event.target.id;
+  id = id.substring(0, id.length - 1);
+  document.getElementById(id).style.visibility = "visible";
+  document.getElementById(id + "I").style.visibility = "hidden";
+  if (document.getElementById("C" + id).checked) {
+    document.getElementById(event.target.id).checked = false
+  }
+  else {
+    parts--;
+    price -= chosenComponents[id].price;
+  }
+
+  wattage -= chosenComponents[id].wattage;
+  update();
+  chosenComponents[id] = "";
+}
+
+function owned(event) {
+  id = event.target.id.substr(1);
+  console.log(document.getElementById(event.target.id).checked);
+  if (document.getElementById(event.target.id).checked) {
+    price -= chosenComponents[id].price;
+    parts--;
+  }
+  else {
+    price += chosenComponents[id].price;
+    parts++;
+  }
+  update();
+}
---
+++ js/partChoosingComponents.js
@@ -0,0 +1,132 @@
+var componentTypeFilter = "";
+var allComponents = new Array();
+
+function loadAllComponents() {
+    allComponents = JSON.parse(data);
+}
+
+function componentFilter(components) {
+    var filteredOut = new Array();
+    for (i = 0; i < components.length; i++) {
+        if (
+            components[i].type == componentTypeFilter ||
+            componentTypeFilter == ""
+        ) {
+            filteredOut.push(components[i]);
+        }
+    }
+    return filteredOut;
+    ks;
+}
+
+function filter(components) {
+    return components;
+}
+
+function addComponentToView(component) {
+    allComponentsNode = document.getElementById("components");
+    componentNode = document.createElement("div");
+    componentNode.setAttribute("class", "element row border-top mx-2");
+
+    imgWrap = document.createElement("div");
+    imgWrap.setAttribute("class", "col-1 my-1");
+
+    imageNode = document.createElement("img");
+    imageNode.setAttribute("class", "image border");
+    //imageNode.setAttribute("src",component.img);
+    imageNode.setAttribute("src", component.img);
+    imageNode.setAttribute("alt", "productImage");
+    imgWrap.appendChild(imageNode);
+
+    nameNode = document.createElement("a");
+    nameNode.setAttribute("href", "./component_page.html");
+    nameNode.setAttribute("style", "text-decoration: none;");
+    nameNode.setAttribute("class", "name col-4 text-dark font-weight-bold align-self-center text-wrap");
+    nameNode.setAttribute("name", "name");
+    textNode = document.createTextNode(component.name);
+    nameNode.appendChild(textNode);
+
+    data1Node = document.createElement("div");
+    data1Node.setAttribute("class", "data1 col-3 align-self-center text-wrap");
+    textNode = document.createTextNode(component.data1);
+    data1Node.appendChild(textNode);
+
+    data2Node = document.createElement("div");
+    data2Node.setAttribute("class", "data2 col-2 align-self-center text-wrap");
+    textNode = document.createTextNode(component.data2);
+    data2Node.appendChild(textNode);
+
+    priceNode = document.createElement("div");
+    priceNode.setAttribute("class", "price col-1 align-self-center text-wrap");
+    textNode = document.createTextNode("$" + component.price);
+    priceNode.appendChild(textNode);
+
+    btnWrap = document.createElement("div");
+    btnWrap.setAttribute("class", "col-1 align-self-center");
+
+    buttonNode = document.createElement("button");
+    buttonNode.setAttribute("class", "addButton btn btn-sm btn-outline-dark text-nowrap");
+    buttonNode.setAttribute("style", "margin-left: -20px");
+    textNode = document.createTextNode("Add to cart");
+    buttonNode.appendChild(textNode);
+    btnWrap.appendChild(buttonNode);
+
+    //componentNode.appendChild(imageNode);
+    componentNode.appendChild(imgWrap);
+    componentNode.appendChild(nameNode);
+    componentNode.appendChild(data1Node);
+    componentNode.appendChild(data2Node);
+    componentNode.appendChild(priceNode);
+    //  componentNode.appendChild(buttonNode);
+    componentNode.appendChild(btnWrap);
+
+    allComponentsNode.appendChild(componentNode);
+}
+
+function clearComponentView() {
+    allComponentsNode = document.getElementById("components");
+    while (allComponentsNode.firstChild) {
+        allComponentsNode.removeChild(allComponentsNode.lastChild);
+    }
+}
+function updateComponentView() {
+    clearComponentView();
+    var filteredOut;
+    filteredOut = componentFilter(allComponents);
+    filteredOut = filter(filteredOut);
+    for (i = 0; i < filteredOut.length; i++) {
+        addComponentToView(filteredOut[i]);
+    }
+}
+
+$(document).ready(function () {
+
+    loadAllComponents();
+    updateComponentView();
+});
+
+
+function uncheckAll(id) {
+    $('input[type="checkbox"]').prop('checked', false);
+    document.getElementById(id).checked = true;
+}
+
+function filterIt(event) {
+    uncheckAll(event.target.id);
+    if (event.target.id == "all") {
+        componentTypeFilter = "";
+        loadAllComponents();
+        updateComponentView();
+    }
+    else if (event.target.id != componentTypeFilter) {
+        componentTypeFilter = event.target.id;
+        loadAllComponents();
+        updateComponentView();
+    }
+    else {
+        uncheckAll("all");
+        componentTypeFilter = "";
+        loadAllComponents();
+        updateComponentView();
+    }
+}
